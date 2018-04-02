41ebaf8945cb1437843b9de7f44a49a2d09c2521
liteng <930372551@qq.com>
2018-04-02 19:49:58 +0800

    添加material-dashboard-react项目。

--- MaterialApp.Web/src/index.js
+++ MaterialApp.Web/src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './admin/Login';
-import Index from './admin/Index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "./assets/css/material-dashboard-react.css";
+
+import indexRoutes from "routes/index.jsx";
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Login />, document.querySelector('#root'));
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} component={prop.component} key={key} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
