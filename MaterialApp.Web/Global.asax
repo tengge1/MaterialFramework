6d6c5192b2d23423c48e17bece62e32c1221bed8
liteng <930372551@qq.com>
2018-04-03 22:07:39 +0800

    WebAPI控制器已经能正常访问。

---
+++ MaterialApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaterialApp.Web.WebApplication" Language="C#" %>
---
+++ MaterialApp.Web/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+using NHibernate;
+using Spring.Web.Mvc;
+using log4net.Config;
+
+namespace MaterialApp.Web
+{
+    /// <summary>
+    /// Web应用程序
+    /// </summary>
+    public class WebApplication : SpringMvcApplication
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public override void Init()
+        {
+            // 开启ApiController的Session功能
+            this.PostAuthenticateRequest += (sender, e) => HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+
+            base.Init();
+        }
+
+        /// <summary>
+        /// 应用程序启动
+        /// </summary>
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // 初始化log4net配置
+            XmlConfigurator.Configure();
+
+            // 记录系统启动日志
+            var log = FileLogHelper.GetLogger(this.GetType());
+            log.Info("系统启动");
+        }
+
+        /// <summary>
+        /// 解决“会话状态已创建一个会话 ID，但由于响应已被应用程序刷新而无法保存它。”错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            var sessionId = Session.SessionID;
+        }
+    }
+}
