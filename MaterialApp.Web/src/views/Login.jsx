279ccd17e95d34149ba216d901cf21502b38bb4c
liteng <930372551@qq.com>
2018-04-10 20:38:02 +0800

    登录界面优化修改。

--- MaterialApp.Web/src/views/Login.jsx
+++ MaterialApp.Web/src/views/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import {
     Card,
     CardHeader,
@@ -10,9 +11,12 @@ import {
     withStyles,
     FormControl,
     FormLabel,
-    Grid,
-    Input
+    Input,
+    InputAdornment,
+    IconButton,
+    InputLabel
 } from 'material-ui';
+import {VisibilityOff, Visibility} from 'material-ui-icons';
 import Cookies from 'js-cookie';
 import withRoot from '../withRoot';
 import cover from '../assets/img/cover.jpeg';
@@ -29,31 +33,38 @@ const styles = {
         backgroundSize: 'cover'
     },
     card: {
-        width: 450,
-        height: 320,
+        width: 420,
+        height: 300,
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-around',
+        justifyContent: 'flex-start',
         alignItems: 'center'
+    },
+    header: {
+        paddingBottom: 0
     },
     avatar: {
         width: '64px',
         height: '64px'
     },
     content: {
-        width: '70%'
+        width: '260px',
+        paddingBottom: '26px'
     },
     action: {
         display: 'flex',
         justifyContent: 'center'
+    },
+    button: {
+        margin: '0 15px'
     }
 };
 
 class Login extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+    state = {
+        showPassword: false
+    };
 
     login = () => {
         Cookies.set('isLogin', true);
@@ -62,39 +73,58 @@ class Login extends React.Component {
             .reload();
     }
 
-    reset = () => {
+    reset = () => {}
 
+    handleShowPassword = () => {
+        this.setState({
+            showPassword: !this.state.showPassword
+        });
+    }
+
+    handleMouseDownPassword = (event) => {
+        event.preventDefault();
     }
 
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
 
-        const avatar = <Avatar src={face} alt={'头像'} className={classes.avatar} />;
+        const avatar = <Avatar src={face} alt={'头像'} className={classes.avatar}/>;
+
+        var passwordAdornment = <InputAdornment position="end">
+            <IconButton
+                onClick={this.handleShowPassword}
+                onMouseDown={this.handleMouseDownPassword}>
+                {this.state.showPassword
+                    ? <VisibilityOff/>
+                    : <Visibility/>}
+            </IconButton>
+        </InputAdornment>;
 
         return (
             <div className={classes.root}>
                 <Card className={classes.card} raised={true} square={false}>
-                    <CardHeader title={''} avatar={avatar} />
+                    <CardHeader title={''} className={classes.header} avatar={avatar}/>
                     <CardContent className={classes.content}>
-                        <Grid container spacing={24}>
-                            <Grid item md={3}>
-                                <FormLabel>用户名</FormLabel>
-                            </Grid>
-                            <Grid item md={9}>
-                                <Input autoFocus={true} name={'username'} required={true} fullWidth={true} />
-                            </Grid>
-                        </Grid>
-                        <Grid container spacing={24}>
-                            <Grid item md={3}>
-                                <FormLabel>密码</FormLabel>
-                            </Grid>
-                            <Grid item md={9}>
-                                <Input name={'password'} required={true} fullWidth={true} />
-                            </Grid>
-                        </Grid>
+                        <FormControl className={classNames(classes.margin, classes.textField)}>
+                            <InputLabel>用户名</InputLabel>
+                            <Input type={'text'} fullWidth={true}/>
+                        </FormControl>
+                        <FormControl className={classNames(classes.margin, classes.textField)}>
+                            <InputLabel>密码</InputLabel>
+                            <Input
+                                type={this.state.showPassword
+                                ? 'text'
+                                : 'password'}
+                                fullWidth={true}
+                                endAdornment={passwordAdornment}/>
+                        </FormControl>
                     </CardContent>
                     <CardActions className={classes.action}>
-                        <Button variant="raised" color="primary" className={classes.button} onClick={this.login}>
+                        <Button
+                            variant="raised"
+                            color="primary"
+                            className={classes.button}
+                            onClick={this.login}>
                             登录
                         </Button>
                         <Button variant="raised" className={classes.button} onClick={this.reset}>
