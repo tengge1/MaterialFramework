c25b9d5375b3d36f3f4fe18a987a5ecb6ce9f225
liteng <930372551@qq.com>
2018-05-07 07:45:01 +0800

    添加产量图片。

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
     Grid, Card, CardHeader, CardContent, List, ListItem, ListItemText, FixedTabs,
-    Controller, AppBar, Toolbar, IconButton
+    Controller, AppBar, Toolbar, IconButton, Text, ECharts
 } from '../../components/Components';
 import { MoreVert } from '../../components/Icons';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
@@ -12,7 +12,11 @@ const styles = theme => ({
         height: '100%',
         padding: 16
     },
-    headerText: {
+    headerAppBar: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    headerToolbar: {
         flex: 1
     }
 });
@@ -24,8 +28,8 @@ class Home extends React.Component {
         return <div className={classes.root} ref={this.rootRef}><Grid container spacing={16}>
             <Grid item xs={4}>
                 <Card>
-                    <AppBar color={'default'}>
-                        <Toolbar className={classes.headerText}>通知公告</Toolbar>
+                    <AppBar color={'default'} className={classes.headerAppBar}>
+                        <Toolbar className={classes.headerToolbar}>通知公告</Toolbar>
                         <IconButton><MoreVert /></IconButton>
                     </AppBar>
                     <List>
@@ -51,17 +55,11 @@ class Home extends React.Component {
             </Grid>
             <Grid item xs={4}>
                 <Card>
-                    <CardHeader>集团新闻</CardHeader>
-                    <CardContent>
-                        <List>
-                            <ListItem>
-                                王宜林与国家应急管理部党组书记黄明会谈
-                            </ListItem>
-                            <ListItem>
-                                中国石油直属团委表彰第三届“十佳青年岗位能手”
-                            </ListItem>
-                        </List>
-                    </CardContent>
+                    <AppBar color={'default'} className={classes.headerAppBar}>
+                        <Toolbar className={classes.headerToolbar}>油田产量</Toolbar>
+                        <IconButton><MoreVert /></IconButton>
+                    </AppBar>
+                    <ECharts option={this.getProductionChange()} />
                 </Card>
             </Grid>
             <Grid item xs={4}>
--- src/views/Home/HomeController.jsx
+++ src/views/Home/HomeController.jsx
@@ -15,6 +15,64 @@ class HomeController {
             this.scrollbar = null;
         }
     }
+
+    getProductionChange() {
+        const year = new Date().getFullYear();
+        return {
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['大庆油田', '胜利油田', '江苏油田', '长庆油田', '克拉玛依油田']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: [year - 6, year - 5, year - 4, year - 3, year - 2, year - 1, year]
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name: '大庆油田',
+                    type: 'line',
+                    stack: '总量',
+                    data: [120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name: '胜利油田',
+                    type: 'line',
+                    stack: '总量',
+                    data: [220, 182, 191, 234, 290, 330, 310]
+                },
+                {
+                    name: '江苏油田',
+                    type: 'line',
+                    stack: '总量',
+                    data: [150, 232, 201, 154, 190, 330, 410]
+                },
+                {
+                    name: '长庆油田',
+                    type: 'line',
+                    stack: '总量',
+                    data: [320, 332, 301, 334, 390, 330, 320]
+                },
+                {
+                    name: '克拉玛依油田',
+                    type: 'line',
+                    stack: '总量',
+                    data: [820, 932, 901, 934, 1290, 1330, 1320]
+                }
+            ]
+        };
+    }
 }
 
 export default HomeController;
